9c128f308074ce4d0a47f8d8f1191e96bcb393f4
ENER90 <renep2010@gmail.com>
2025-02-01 17:16:14 -0300

    Probando validacion metodo POST.

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-require(`dotenv`).config();
-const personaRouter = require(`./routes/persona`);
+require('dotenv').config();
+const personaRouter = require('./routes/persona');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(`/personas`, personaRouter);
+
+//Middleware para gestionar Personas
+app.use('/personas', personaRouter);
+
+// Middleware para manejar errores de JSON malformado
+app.use((error, _req, res, next) => {
+  if(error instanceof SyntaxError && error.status === 400 && 'body' in error){
+    return res.status(400).json({ error: "Invalid JSON"});
+  }
+  next();
+});
 
 //inicializar servidor
 app.listen(port, () => {
--- routes/persona.js
+++ routes/persona.js
@@ -1,36 +1,89 @@
-const express = require(`express`);
-const connectDB = require(`../db`);
+const express = require('express');
+const connectDB = require('../db');
 connectDB();
-const Persona = require(`../models/persona`);
+const Persona = require('../models/persona');
 const personaRouter = express.Router(); 
 
+function validarPersona(req, res, next){
+    console.log('aqui');
+    const {name, age} = req.body;
+    if (!name || typeof name !== 'string' || name.trim() === ''){
+        return res.status(400).json({error: 'The name can`t be empty and must be a string.'})
+    }
+    
+    if (!age || isNaN(Number(age)) || Number(age) > 100 || Number(age) <= 0){
+        return res.status(400).json({error: 'The age cannot be empty and must be a valid number between 1 and 100.'})
+    }
+
+    next();
+}
+
+function validarActualizacion(req, res, next) {
+    const {name, age} = req.body;
+    const { id } = req.params;
+
+    // Validar el formato del ID de MongoDB
+    if (!id || !id.match(/^[0-9a-fA-F]{24}$/)) {
+        return res.status(400).json({ error: 'invalid ID format'});
+    }
+
+    // Validar el nombre (si se envía)
+    if(name !== undefined){
+        if(name !== 'string' || name.trim()=== ''){
+            return res.status(400).json({ error: 'The name cannot be empty and must be a string'});
+        }
+    }
+    
+    // Validar la edad (si se envía)
+    if(age !== undefined){
+        if(!age || isNaN(Number(age)) || Number(age) > 100 || Number(age) <= 0){
+            return res.status(400).json({error: 'The age cannot be empty and must be a valid number between 1 and 100.'})
+        }
+    };
+
+    next();
+}
 //listar personas
-personaRouter.get(`/`, async (_req, res) => {
+personaRouter.get('/', async (_req, res) => {
+    
     const personas = await Persona.find()
-    res.json(personas);
+    res.json(personas);    
+    res.status(500).json({error: 'Error to try seve a person'});  
 });
 
 //buscar personas por id
-personaRouter.get(`/:id`, async (req, res) => {
+personaRouter.get('/:id', async (req, res) => {
     const persona = await Persona.findById(req.params.id);
     res.json(persona);
 });
 
 //crear nueva persona
-personaRouter.post(`/`, async (req, res) => {
-    const newPersona = new Persona(req.body);
-    await newPersona.save();
-    res.status(201).json(newPersona);
+personaRouter.post('/', validarPersona, async (req, res) => {
+    try{
+        const newPersona = new Persona(req.body);
+        await newPersona.save();
+        res.status(201).json(newPersona);
+    }catch (error) {
+        res.status(500).json({error: 'Error trying to seve a person'});
+    }
 });
 
 //editar persona por id
-personaRouter.put(`/:id`, async (req, res) => {
-    const updatePersona = await Persona.findByIdAndUpdate(req.params.id, req.body);
-    res.json(updatePersona);
+personaRouter.put('/:id', validarActualizacion, async (req, res) => {
+    try{
+        const updatePersona = await Persona.findByIdAndUpdate(req.params.id, req.body);
+
+        if(!updatePersona){
+            return res.status(404).json({ error: 'Person not found'})
+        }
+        res.json(updatePersona);
+    }catch (error) {
+        res.status(500).json({error: 'Error trying to edit a person'});
+    }
 });
 
 //eliminar persona por id
-personaRouter.delete(`/:id`, async (req, res) => {
+personaRouter.delete('/:id', async (req, res) => {
     await Persona.findByIdAndDelete(req.params.id);
     res.send();
 });
