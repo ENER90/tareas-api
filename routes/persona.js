95b24c204d9b601ae92ae67ed01926e3ef2b4139
ENER90 <renep2010@gmail.com>
2025-02-05 18:33:11 -0300

    Sacanco las validaciones del archivo persona en routes, creando el directorio validations para persona

--- routes/persona.js
+++ routes/persona.js
@@ -1,62 +1,9 @@
 const express = require('express');
-const connectDB = require('../db');
+const connectDB = require('../db'); // Why does it happen if I already export connectDB in db.js?
 connectDB();
 const Persona = require('../models/persona');
 const personaRouter = express.Router(); 
-
-// validacion para el POST
-function validarPersona(req, res, next){
-    console.log('aqui');
-    const {name, age} = req.body;
-    if (!name || typeof name !== 'string' || name.trim() === ''){
-        return res.status(400).json({error: 'The name can`t be empty and must be a string.'})
-    }
-    
-    if (!age || isNaN(Number(age)) || Number(age) > 100 || Number(age) <= 0){
-        return res.status(400).json({error: 'The age cannot be empty and must be a valid number between 1 and 100.'})
-    }
-
-    next();
-}
-
-// validacion para el PUT
-function validarActualizacion(req, res, next) {
-    const {name, age} = req.body;
-    const { id } = req.params;
-
-    // Validar el formato del ID de MongoDB
-    if (!id || !id.match(/^[0-9a-fA-F]{24}$/)) {
-        return res.status(400).json({ error: 'invalid ID format'});
-    }
-
-    // Validar el nombre (si se envía)
-    if(name !== undefined){
-        if(name !== 'string' || name.trim()=== ''){
-            return res.status(400).json({ error: 'The name cannot be empty and must be a string'});
-        }
-    }
-    
-    // Validar la edad (si se envía)
-    if(age !== undefined){
-        if(!age || isNaN(Number(age)) || Number(age) > 100 || Number(age) <= 0){
-            return res.status(400).json({error: 'The age cannot be empty and must be a valid number between 1 and 100.'})
-        }
-    };
-
-    next();
-}
-
-//validacion para el GET por ID
-function validarID(req, res, next) {
-    const { id } = req.params;
-
-    // Validar el formato del ID de MongoDB
-    if (!id || !id.match(/^[0-9a-fA-F]{24}$/)) {
-        return res.status(400).json({ error: 'invalid ID format'});
-    }
-
-    next();
-}
+const validation = require('../validations/persona')
 
 //listar personas
 personaRouter.get('/', async (_req, res) => {
@@ -70,7 +17,7 @@ personaRouter.get('/', async (_req, res) => {
 });
 
 //buscar personas por id
-personaRouter.get('/:id', validarID, async (req, res) => {
+personaRouter.get('/:id', validation.validarID, async (req, res) => {
     try {
         const persona = await Persona.findById(req.params.id);
 
@@ -86,7 +33,7 @@ personaRouter.get('/:id', validarID, async (req, res) => {
 });
 
 //crear nueva persona
-personaRouter.post('/', validarPersona, async (req, res) => {
+personaRouter.post('/', validation.validarPersona, async (req, res) => {
     try{
         const newPersona = new Persona(req.body);
         await newPersona.save();
@@ -97,7 +44,7 @@ personaRouter.post('/', validarPersona, async (req, res) => {
 });
 
 //editar persona por id
-personaRouter.put('/:id', validarActualizacion, async (req, res) => {
+personaRouter.put('/:id', validation.validarActualizacion, async (req, res) => {
     try{
         const updatePersona = await Persona.findByIdAndUpdate(req.params.id, req.body);
 
@@ -111,7 +58,7 @@ personaRouter.put('/:id', validarActualizacion, async (req, res) => {
 });
 
 //eliminar persona por id
-personaRouter.delete('/:id', validarID, async (req, res) => {
+personaRouter.delete('/:id', validation.validarID, async (req, res) => {
     try {
         const deletePersona = await Persona.findByIdAndDelete(req.params.id);
 
